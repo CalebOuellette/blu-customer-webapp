8e5a27f6e8dd17896b2fd2138b03a654bb86b34d
CalebOuellette <Caleb.Ouellette@gmail.com>
2017-08-05 23:10:06 -0700

    added cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProductSearchComponent } from './product-search/product-search.component';
-
+import { CartComponent } from './cart/cart.component';
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   {path: 'home', component: ProductSearchComponent},
@@ -26,7 +26,8 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     LoginComponent,
-    ProductSearchComponent
+    ProductSearchComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
