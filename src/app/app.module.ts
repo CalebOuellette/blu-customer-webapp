691504add071a3a2b53c62a09f07662fe02ddadf
CalebOuellette <Caleb.Ouellette@gmail.com>
2017-08-05 21:11:45 -0700

    login hook up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ProductSearchComponent } from './product-search/product-search.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-
+  {path: 'home', component: ProductSearchComponent},
   {
     path: '',
     redirectTo: '/login',
@@ -24,14 +25,16 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProductSearchComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule, // imports firebase/database, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: Observable<firebase.User>;
+
+  public email: string;
+  public password: string;
+
+  public errorText: string;
+
+  constructor(public afAuth: AngularFireAuth, public router: Router) {
+    this.user = afAuth.authState;
+  }
 
   ngOnInit() {
   }
+
+
+  login() {
+    this.errorText = "";
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password).then(success => {
+      this.router.navigate(['/home']);
+    }, fail => {
+      this.errorText = "Yikes, something went really really wrong. Try again?";
+    })
+  }
+
+  signUp() {
+    this.errorText = "";
+    this.afAuth.auth.createUserWithEmailAndPassword(this.email, this.password).then(success => {
+      this.router.navigate(['/home']);
+    }, fail => {
+      this.errorText = "Yikes, something went really really wrong. Try again?";
+    })
+  }
+
 
 }
