7a2edba64b3c00ec4b39c3d88549233b4b0c7411
CalebOuellette <Caleb.Ouellette@gmail.com>
2017-08-08 18:09:42 -0700

    updated env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+
 export const environment = {
-  production: true
+  production: true,
+   firebase: {
+    apiKey: 'AIzaSyDobwf-qk7GDNXECf4YqfQ2gnSaNd8cKDc',
+    authDomain: 'bludelivery-f28c2.firebaseapp.com',
+    databaseURL: 'https://bludelivery-f28c2.firebaseio.com/',
+    projectId: 'bludelivery-f28c2',    
+  }
 };
+
