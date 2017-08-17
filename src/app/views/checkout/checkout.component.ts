be5a714b4ce142c957a21cf16840c70f99e65d65
CalebOuellette <Caleb.Ouellette@gmail.com>
2017-08-16 22:37:46 -0700

    Added saving.

--- src/app/views/checkout/checkout.component.ts
+++ src/app/views/checkout/checkout.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
 import { CartService } from '../../services/cart.service';
-import { OrderProps, CustomerProps } from '../../../../blu-classes';
+import { OrderProps, CustomerProps, Customer, Order } from '../../../../blu-classes';
 import { Router } from '@angular/router';
 import { emailMask } from 'text-mask-addons';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -21,13 +21,14 @@ export class CheckoutComponent implements OnInit {
 
   public message: string;
 
-
+  public selectedPaymentType: string = "";
   //Customer Info
   public customer: CustomerProps = new CustomerProps();
   public order: OrderProps = new OrderProps();
 
   public deliveryLocation: string = "";
 
+  @ViewChild('infoForm') infoForm;
 
   constructor(public fireDb: AngularFireDatabase, private _zone: NgZone, public cart: CartService, public router: Router) { }
 
@@ -40,11 +41,10 @@ export class CheckoutComponent implements OnInit {
   public aEmailMask = emailMask;
 
   public aPhoneMask = function (rawValue) {
-    // add logic to generate your mask array
     return ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
   }
 
-  
+
 
   getToken() {
     this.message = 'Loading...';
@@ -68,8 +68,16 @@ export class CheckoutComponent implements OnInit {
   }
 
 
-  confirmOrder(){
+  confirmOrder() {
+    this.fireDb.list(Customer.dbAddress).push(this.customer).then((success) => {
+      this.order.customerID = success.key;
+      this.order.paymentType = this.selectedPaymentType;
+      this.order.to = this.deliveryLocation;
+      this.order.orderTime = new Date();      
 
+    }, fail => {
+      console.warn("Error Creating Customer");      
+    });
   }
 
 }
